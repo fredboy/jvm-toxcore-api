875842aa2a638430881e08cfd5208af307dcd0ba
fredboy <fredboy@protonmail.com>
2023-12-10 17:47:15 +0700

    kotlin

--- src/main/java/im/tox/tox4j/av/callbacks/VideoBitRateCallback.kt
+++ src/main/java/im/tox/tox4j/av/callbacks/VideoBitRateCallback.kt
@@ -7,13 +7,17 @@ import im.tox.tox4j.core.data.ToxFriendNumber
  * The event is triggered when the network becomes too saturated for current
  * bit rates at which point core suggests new bit rates.
  */
-trait VideoBitRateCallback[ToxCoreState] {
+interface VideoBitRateCallback<ToxCoreState> {
+
   /**
    * @param friendNumber The friend number of the friend for which to set the
    *                     video bit rate.
    * @param videoBitRate Suggested maximum video bit rate in Kb/sec.
    */
-  def videoBitRate(
-    friendNumber: ToxFriendNumber, videoBitRate: BitRate
-  )(state: ToxCoreState): ToxCoreState = state
+  fun videoBitRate(
+          friendNumber: ToxFriendNumber,
+          videoBitRate: BitRate,
+          state: ToxCoreState
+  ) = state
+
 }

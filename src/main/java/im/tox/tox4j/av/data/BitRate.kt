c08e85552158f72d4c3a2b9d596ccba2aa82ac3c
fredboy <fredboy@protonmail.com>
2023-12-10 17:20:54 +0700

    BitRate

--- src/main/java/im/tox/tox4j/av/data/BitRate.kt
+++ src/main/java/im/tox/tox4j/av/data/BitRate.kt
@@ -2,14 +2,21 @@ package im.tox.tox4j.av.data
 
 import im.tox.core.typesafe.BoundedIntCompanion
 
-final case class BitRate private (value: Int) extends AnyVal
+data class BitRate internal constructor(val value: Int) {
 
-case object BitRate extends BoundedIntCompanion[BitRate](-1, Int.MaxValue) {
+  companion object : BoundedIntCompanion<BitRate>(-1, Int.MAX_VALUE) {
 
-  val Unchanged: BitRate = new BitRate(-1)
-  val Disabled: BitRate = new BitRate(0)
+    val unchanged = BitRate(-1)
+    val disabled = BitRate(0)
 
-  override def unsafeFromInt(value: Int): BitRate = new BitRate(value)
-  override def toInt(self: BitRate): Int = self.value
+    override fun unsafeFromInt(value: Int): BitRate {
+      return BitRate(value)
+    }
+
+    override fun toInt(self: BitRate): Int {
+      return self.value
+    }
+
+  }
 
 }

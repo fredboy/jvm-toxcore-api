cccef6b1b7f94b8a8a2231fcdceb4c4ec0074c41
fredboy <fredboy@protonmail.com>
2023-12-10 13:50:38 +0700

    ModuleCompanion.kt IntCompanion.kt

--- src/main/java/im/tox/core/ModuleCompanion.kt
+++ src/main/java/im/tox/core/ModuleCompanion.kt
@@ -2,7 +2,7 @@ package im.tox.core
 
 import im.tox.core.typesafe.Security
 
-abstract class ModuleCompanion<T, in S : Security> {
+abstract class ModuleCompanion<in T, in S : Security> {
 
   abstract fun equals(a: T?, b: T?): Boolean
 

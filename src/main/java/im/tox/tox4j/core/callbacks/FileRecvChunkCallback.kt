790edd7355b70de18d5cc5ae4f1f866fc45ea6a4
fredboy <fredboy@protonmail.com>
2023-12-10 22:29:55 +0700

    kotlin

--- src/main/java/im/tox/tox4j/core/callbacks/FileRecvChunkCallback.kt
+++ src/main/java/im/tox/tox4j/core/callbacks/FileRecvChunkCallback.kt
@@ -7,7 +7,7 @@ import org.jetbrains.annotations.NotNull
  * This event is first triggered when a file transfer request is received, and
  * subsequently when a chunk of file data for an accepted request was received.
  */
-trait FileRecvChunkCallback[ToxCoreState] {
+interface FileRecvChunkCallback<ToxCoreState> {
   /**
    * When length is 0, the transfer is finished and the client should release the
    * resources it acquired for the transfer. After a call with length = 0, the
@@ -22,7 +22,9 @@ trait FileRecvChunkCallback[ToxCoreState] {
    * @param position The file position of the first byte in data.
    * @param data A byte array containing the received chunk.
    */
-  def fileRecvChunk(
-    friendNumber: ToxFriendNumber, fileNumber: Int, position: Long, @NotNull data: Array[Byte]
-  )(state: ToxCoreState): ToxCoreState = state
+  fun fileRecvChunk(friendNumber: ToxFriendNumber,
+                    fileNumber: Int,
+                    position: Long,
+                    data: ByteArray,
+                    state: ToxCoreState): ToxCoreState = state
 }

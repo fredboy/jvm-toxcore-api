73868a668101be1bbed0d87d6f8aa6b9b4b8c065
fredboy <fredboy@protonmail.com>
2023-12-10 13:10:19 +0700

    toxeventlistener

--- src/main/java/im/tox/tox4j/ToxEventListener.kt
+++ src/main/java/im/tox/tox4j/ToxEventListener.kt
@@ -3,6 +3,4 @@ package im.tox.tox4j
 import im.tox.tox4j.av.callbacks.ToxAvEventListener
 import im.tox.tox4j.core.callbacks.ToxCoreEventListener
 
-trait ToxEventListener[ToxCoreState]
-  extends ToxCoreEventListener[ToxCoreState]
-  with ToxAvEventListener[ToxCoreState]
+interface ToxEventListener<ToxCoreState> : ToxCoreEventListener<ToxCoreState>, ToxAvEventListener<ToxCoreState>

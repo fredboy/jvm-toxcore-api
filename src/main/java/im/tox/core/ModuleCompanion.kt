a48e4b837baf94a0020411e074e43dc08c892bc5
fredboy <fredboy@protonmail.com>
2023-12-10 14:35:38 +0700

    Port.kt, Remove core error

--- src/main/java/im/tox/core/ModuleCompanion.kt
+++ src/main/java/im/tox/core/ModuleCompanion.kt
@@ -4,6 +4,6 @@ import im.tox.core.typesafe.Security
 
 abstract class ModuleCompanion<in T, in S : Security> {
 
-  abstract fun equals(a: T?, b: T?): Boolean
+  abstract fun equals(a: T, b: T): Boolean
 
 }
--- src/main/java/im/tox/core/typesafe/IntCompanion.kt
+++ src/main/java/im/tox/core/typesafe/IntCompanion.kt
@@ -6,8 +6,8 @@ abstract class IntCompanion<T : Any> : ModuleCompanion<T, Security.Sensitive>() 
 
   protected abstract fun fromInt(value: Int): T?
 
-  protected abstract fun toInt(self: T?): Int
+  protected abstract fun toInt(self: T): Int
 
-  override fun equals(a: T?, b: T?): Boolean = toInt(a) == toInt(b)
+  override fun equals(a: T, b: T): Boolean = toInt(a) == toInt(b)
 
 }

60c5b36662626dc1c1af38d4b07b2df973ac2e6c
fredboy <fredboy@protonmail.com>
2023-12-10 17:45:38 +0700

    kotlin

--- src/main/java/im/tox/tox4j/av/callbacks/CallStateCallback.kt
+++ src/main/java/im/tox/tox4j/av/callbacks/CallStateCallback.kt
@@ -1,22 +1,24 @@
 package im.tox.tox4j.av.callbacks
 
-import java.util
-
 import im.tox.tox4j.av.enums.ToxavFriendCallState
 import im.tox.tox4j.core.data.ToxFriendNumber
-import org.jetbrains.annotations.NotNull
+import java.util.*
 
 /**
  * Called when the call state changes.
  */
-trait CallStateCallback[ToxCoreState] {
+interface CallStateCallback<ToxCoreState> {
+
   /**
    * @param friendNumber The friend number this call state change is for.
    * @param callState A set of ToxCallState values comprising the new call state.
    *                  Although this is a Collection (therefore might actually be a List), this is
-   *                  effectively a Set. Any [[ToxavFriendCallState]] value is contained exactly 0 or 1 times.
+   *                  effectively a Set. Any [ToxavFriendCallState] value is contained exactly 0 or 1 times.
    */
-  def callState(
-    friendNumber: ToxFriendNumber, @NotNull callState: util.EnumSet[ToxavFriendCallState]
-  )(state: ToxCoreState): ToxCoreState = state
+  fun callState(
+          friendNumber: ToxFriendNumber,
+          callState: EnumSet<ToxavFriendCallState>,
+          state: ToxCoreState
+  ) = state
+
 }

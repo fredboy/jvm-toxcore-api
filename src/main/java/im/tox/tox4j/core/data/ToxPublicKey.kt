649f7ed8eedccaef4397271c4a747fc6a8f8e2a6
fredboy <fredboy@protonmail.com>
2023-12-10 22:23:59 +0700

    kotlin

--- src/main/java/im/tox/tox4j/core/data/ToxPublicKey.kt
+++ src/main/java/im/tox/tox4j/core/data/ToxPublicKey.kt
@@ -1,19 +1,25 @@
 package im.tox.tox4j.core.data
 
-import im.tox.core.typesafe.{ KeyCompanion, Security }
+import im.tox.core.typesafe.KeyCompanion
+import im.tox.core.typesafe.Security
 import im.tox.tox4j.crypto.ToxCryptoConstants
 
-@SuppressWarnings(Array("org.wartremover.warts.ArrayEquals"))
-final case class ToxPublicKey private (value: Array[Byte]) extends AnyVal {
-  def toHexString: String = ToxPublicKey.toHexString(this)
-  override def toString: String = s"$productPrefix($toHexString)"
-}
+data class ToxPublicKey internal constructor(val value: ByteArray) {
 
-case object ToxPublicKey extends KeyCompanion[ToxPublicKey, Security.NonSensitive](
-  ToxCryptoConstants.PublicKeyLength,
-  _.value
-) {
+  fun toHexString() = ToxPublicKey.toHexString(this)
 
-  override def unsafeFromValue(value: Array[Byte]): ToxPublicKey = new ToxPublicKey(value)
+  override fun toString(): String {
+    return "(${toHexString()})"
+  }
 
+  companion object : KeyCompanion<ToxPublicKey, Security.Sensitive>(
+          ToxCryptoConstants.publicKeyLength,
+          ToxPublicKey::value
+  ) {
+
+    override fun unsafeFromValue(value: ByteArray): ToxPublicKey {
+      return ToxPublicKey(value)
+    }
+
+  }
 }

283551e3a401c2a73569158ed6c6e710984d7a73
fredboy <fredboy@protonmail.com>
2023-12-10 17:27:26 +0700

    kotlin

--- src/main/java/im/tox/tox4j/av/data/SamplingRate.kt
+++ src/main/java/im/tox/tox4j/av/data/SamplingRate.kt
@@ -2,24 +2,30 @@ package im.tox.tox4j.av.data
 
 import im.tox.core.typesafe.DiscreteIntCompanion
 
-final case class SamplingRate private (value: Int) extends AnyVal
+data class SamplingRate internal constructor(val value: Int) {
 
-// scalastyle:off magic.number
-case object SamplingRate extends DiscreteIntCompanion[SamplingRate](
-  8000,
-  12000,
-  16000,
-  24000,
-  48000
-) {
+  companion object : DiscreteIntCompanion<SamplingRate>(
+          8000,
+          12000,
+          16000,
+          24000,
+          48000
+  ) {
 
-  val Rate8k: SamplingRate = new SamplingRate(values(0))
-  val Rate12k: SamplingRate = new SamplingRate(values(1))
-  val Rate16k: SamplingRate = new SamplingRate(values(2))
-  val Rate24k: SamplingRate = new SamplingRate(values(3))
-  val Rate48k: SamplingRate = new SamplingRate(values(4))
+    val rate8k = SamplingRate(values[0])
+    val rate12k = SamplingRate(values[1])
+    val rate16k = SamplingRate(values[2])
+    val rate24k = SamplingRate(values[3])
+    val rate48k = SamplingRate(values[4])
 
-  override def unsafeFromInt(value: Int): SamplingRate = new SamplingRate(value)
-  override def toInt(self: SamplingRate): Int = self.value
+    override fun unsafeFromInt(value: Int): SamplingRate {
+      return SamplingRate(value)
+    }
+
+    override fun toInt(self: SamplingRate): Int {
+      return self.value
+    }
+
+  }
 
 }

d60a8466bd2f99b4fdb0784d7fccfd823b857a6e
fredboy <fredboy@protonmail.com>
2023-12-10 22:42:15 +0700

    kotlin

--- src/main/java/im/tox/tox4j/core/callbacks/FriendTypingCallback.kt
+++ src/main/java/im/tox/tox4j/core/callbacks/FriendTypingCallback.kt
@@ -5,12 +5,13 @@ import im.tox.tox4j.core.data.ToxFriendNumber
 /**
  * This event is triggered when a friend starts or stops typing.
  */
-trait FriendTypingCallback[ToxCoreState] {
+interface FriendTypingCallback<ToxCoreState> {
   /**
    * @param friendNumber The friend number of the friend who started or stopped typing.
    * @param isTyping Whether the friend started (true) or stopped (false) typing.
    */
-  def friendTyping(
-    friendNumber: ToxFriendNumber, isTyping: Boolean
-  )(state: ToxCoreState): ToxCoreState = state
+  fun friendTyping(
+          friendNumber: ToxFriendNumber,
+          isTyping: Boolean,
+          state: ToxCoreState): ToxCoreState = state
 }

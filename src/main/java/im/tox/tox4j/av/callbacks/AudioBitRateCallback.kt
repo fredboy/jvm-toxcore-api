1aaf213433939a4f4feb8d3ae145536a418ae7e4
fredboy <fredboy@protonmail.com>
2023-12-10 17:41:57 +0700

    kotlin

--- src/main/java/im/tox/tox4j/av/callbacks/AudioBitRateCallback.kt
+++ src/main/java/im/tox/tox4j/av/callbacks/AudioBitRateCallback.kt
@@ -7,7 +7,7 @@ import im.tox.tox4j.core.data.ToxFriendNumber
  * The event is triggered when the network becomes too saturated for current
  * bit rates at which point core suggests new bit rates.
  */
-interface AudioBitrateCallback<ToxCoreState> {
+interface AudioBitRateCallback<ToxCoreState> {
 
   /**
    * @param friendNumber The friend number of the friend for which to set the

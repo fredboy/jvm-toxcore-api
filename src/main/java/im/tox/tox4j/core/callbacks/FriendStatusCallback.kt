e9a9bc98f969f7e76be702ea9941adbeaa8d2e8b
fredboy <fredboy@protonmail.com>
2023-12-10 22:40:37 +0700

    kotlin

--- src/main/java/im/tox/tox4j/core/callbacks/FriendStatusCallback.kt
+++ src/main/java/im/tox/tox4j/core/callbacks/FriendStatusCallback.kt
@@ -2,17 +2,16 @@ package im.tox.tox4j.core.callbacks
 
 import im.tox.tox4j.core.data.ToxFriendNumber
 import im.tox.tox4j.core.enums.ToxUserStatus
-import org.jetbrains.annotations.NotNull
 
 /**
  * This event is triggered when a friend changes their user status.
  */
-trait FriendStatusCallback[ToxCoreState] {
+interface FriendStatusCallback<ToxCoreState> {
   /**
    * @param friendNumber The friend number of the friend whose user status changed.
    * @param status The new user status.
    */
-  def friendStatus(
-    friendNumber: ToxFriendNumber, @NotNull status: ToxUserStatus
-  )(state: ToxCoreState): ToxCoreState = state
+  fun friendStatus(friendNumber: ToxFriendNumber,
+                   status: ToxUserStatus,
+                   state: ToxCoreState): ToxCoreState = state
 }

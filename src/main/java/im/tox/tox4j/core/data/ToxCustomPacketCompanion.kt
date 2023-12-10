2c46a257a04d29115fb4c8968d174a6707db9972
fredboy <fredboy@protonmail.com>
2023-12-10 21:53:28 +0700

    kotlin

--- src/main/java/im/tox/tox4j/core/data/ToxCustomPacketCompanion.kt
+++ src/main/java/im/tox/tox4j/core/data/ToxCustomPacketCompanion.kt
@@ -1,33 +1,35 @@
 package im.tox.tox4j.core.data
 
-import im.tox.core.error.CoreError
+import com.fernandocejas.sample.core.functional.Either
 import im.tox.core.typesafe.VariableSizeByteArrayCompanion
-import im.tox.core.typesafe.\/
 import im.tox.tox4j.core.ToxCoreConstants
 
-abstract class ToxCustomPacketCompanion[T <: AnyVal](
-    val MinPacketId: Int,
-    val MaxPacketId: Int,
-    toValue: T => Array[Byte]
-) extends VariableSizeByteArrayCompanion[T](ToxCoreConstants.MaxCustomPacketSize, toValue) {
+abstract class ToxCustomPacketCompanion<T : Any>(
+        val minPacketId: Int,
+        val maxPacketId: Int,
+        toValue: (T) -> ByteArray
+) : VariableSizeByteArrayCompanion<T>(ToxCoreConstants.maxCustomPacketLength, toValue) {
 
-  require(MinPacketId <= MaxPacketId)
+  init {
+    require(minPacketId <= maxPacketId)
+  }
 
-  override protected final def validate: Validator = super.validate { value =>
-    if (value.isEmpty) {
-      Some(CoreError.InvalidFormat("Empty custom packet"))
+  override fun validate(value: ByteArray): Exception? {
+    return if (value.isEmpty()) {
+      IllegalArgumentException("Empty custom packet")
     } else {
-      val packetId = value(0) & 0xff
-      if (!(MinPacketId to MaxPacketId contains packetId)) {
-        Some(CoreError.InvalidFormat(s"Invalid packet id: $packetId; should be $MinPacketId <= id <= $MaxPacketId"))
+      val packetId = value.first().toInt() and 0xff
+
+      if (packetId !in minPacketId..maxPacketId) {
+        IllegalArgumentException("Invalid packet id: $packetId; should be $minPacketId <= id <= $maxPacketId")
       } else {
-        None
+        null
       }
     }
   }
 
-  final def fromByteArray(packetId: Int, value: Array[Byte]): CoreError \/ T = {
-    fromValue(packetId.toByte +: value)
+  fun fromByteArray(packedId: Int, value: ByteArray): Either<Exception, T> {
+    return fromValue(ByteArray(value.size + 1) { if (it == 0) packedId.toByte() else value[ it - 1] })
   }
 
 }
